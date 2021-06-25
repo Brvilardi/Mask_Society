dfedb22fa4f8f4bc16ce1e772c0537ec0f806d04
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-06-24 22:24:54 -0300

    Corrigido javadoc

--- src/bueno/vilardi/bruno/models/Membro.java
+++ src/bueno/vilardi/bruno/models/Membro.java
@@ -9,8 +9,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/*
-Classe Membro, que representa o básico de qualquer membro
+
+/**
+ * Classe Membro, que representa o básico de qualquer membro
  */
 public abstract class Membro implements Apresentacao, PostarMensagem {
     // Atributos gerais de qualquer Membro
@@ -20,18 +21,25 @@ public abstract class Membro implements Apresentacao, PostarMensagem {
     protected String assinaturaRegular;
     protected String assinaturaExtra;
 
+    /**
+     * Contrutor da classe Membro
+     * @param username nome do usuario
+     * @param senha senha do usuario
+     * @param role funcao do usuario
+     */
     public Membro(String username, String senha, Funcao role) {
         this.senha = senha;
         this.username = username;
         this.role = role;
     }
 
-    /*
-            Método para saber qual é o horário atual (REGULAR ou EXTRA)
-            retorna o horário atual do sistema
-    */
+
+    /**
+     * Método para saber qual é o horário atual (REGULAR ou EXTRA)
+      * @return Horário atual do sistema
+     */
     public Horario getHorario(){
-        return null; //TODO pegar horário do sistema
+        return null; //TO_DO pegar horário do sistema
     }
 
 
